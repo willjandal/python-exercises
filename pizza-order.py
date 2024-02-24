5f6172867415070c78e714157781f1dbfcae424e
JV <andal.jvwilliam@gmail.com>
2024-02-24 21:17:51 +0400

    test input functionality

--- pizza-order.py
+++ pizza-order.py
@@ -1,12 +1,12 @@
 print("Thank you for choosing Python Pizza Deliveries!")
 
-
-
-size = input() #What size pizza do you want? S, M, or L
-add_pepperoni = input() # Do you want to pepperoni? Y or N
-extra_cheese = input() #Do you want extra cheese? Y or N
-
+size = input("What size of pizza do you want? S, M, or L: ") #What size pizza do you want? S, M, or L
+add_pepperoni = input("Do you want to add pepperoni? [Y or N]: ") # Do you want to pepperoni? Y or N
+extra_cheese = input("Do you want to add extra cheese? [Y or N]: ") #Do you want extra cheese? Y or N
 
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
+print(f"Pizza Size: {size}")
+print(f"Additional Pepperoni: {add_pepperoni}")
+print(f"Additional Cheese: {extra_cheese}")
