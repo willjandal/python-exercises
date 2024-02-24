7b86ad18a6fa437e1777c610fe53b58c55e2eb31
JV <andal.jvwilliam@gmail.com>
2024-02-24 18:55:40 +0400

    first commit

---
+++ pizza-order.py
@@ -0,0 +1,8 @@
+print("Thank you for choosing Python Pizza Deliveries!")
+
+
+
+size = input() #What size pizza do you want? S, M, or L
+add_pepperoni = input() # Do you want to pepperoni? Y or N
+extra_cheese = input() #Do you want extra cheese? Y or N
+
