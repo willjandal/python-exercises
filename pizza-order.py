14f68168e52776108392df55eb291b33f63fd92f
JV <andal.jvwilliam@gmail.com>
2024-02-24 18:58:52 +0400

    second commit

--- pizza-order.py
+++ pizza-order.py
@@ -6,3 +6,4 @@ size = input() #What size pizza do you want? S, M, or L
 add_pepperoni = input() # Do you want to pepperoni? Y or N
 extra_cheese = input() #Do you want extra cheese? Y or N
 
+#added a single line of code to test connection with github
