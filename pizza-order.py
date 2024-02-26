f598ace61b1a540378991165137b56db71bb982f
jv.macbook <andal.jvwilliam@gmail.com>
2024-02-26 22:56:27 +0400

    added price lis

--- pizza-order.py
+++ pizza-order.py
@@ -1,8 +1,16 @@
 print("Thank you for choosing Python Pizza Deliveries!")
 
+#price list
+#Small Pizza (S) = 15
+#Medium Pizza (M) = 20
+#Large Pizza (L) = 25
+#Additional pepperoni = 2
+#Additional cheese = 1
+
 size = input("What size of pizza do you want? S, M, or L: ") #What size pizza do you want? S, M, or L
 add_pepperoni = input("Do you want to add pepperoni? [Y or N]: ") # Do you want to pepperoni? Y or N
 extra_cheese = input("Do you want to add extra cheese? [Y or N]: ") #Do you want extra cheese? Y or N
+
 
 # 🚨 Don't change the code above 👆
 
@@ -10,3 +18,9 @@ extra_cheese = input("Do you want to add extra cheese? [Y or N]: ") #Do you want
 print(f"Pizza Size: {size}")
 print(f"Additional Pepperoni: {add_pepperoni}")
 print(f"Additional Cheese: {extra_cheese}")
+
+
+if size == "S" or "s":
+    print(15)
+else:
+    print("Hello World")
