0fde796673bcb2def2b3859790e55edc0ae4124a
JV <andal.jvwilliam@gmail.com>
2024-03-17 21:15:54 +0400

    re-commit changes

--- love-calculator.py
+++ love-calculator.py
@@ -43,12 +43,4 @@ lower_names = combine_names.lower()
 
 countFirstName(lower_names)
 
-#if final_score < 10:
-#    print(f"Your score is {final_score}, you go together like coke and mentos.")
-#elif final_score > 90:
-#    print(f"Your score is {final_score}, you go together like coke and mentos.")
-#elif final_score < 50 and final_score > 40:
-#    print(f"Your score is {final_score}, you are alright together.")
-#else:
-#    print(f"Your score is {final_score}.")
-
+#hello
